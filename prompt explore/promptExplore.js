e8048fd087cc87bf0fea1b4051b8191e06f1acb5
shamshul-haque <shamshul.haque.dev@gmail.com>
2023-09-05 19:48:36 +0600

    explore prompt

---
+++ prompt explore/promptExplore.js
@@ -0,0 +1,13 @@
+const a = () => {
+  alert("asos naki?");
+};
+
+const b = () => {
+  const res = confirm("thik asos?");
+  res === true ? alert("alhamdulillah") : alert("be safe");
+};
+
+const c = () => {
+  const res = prompt("ami astesi");
+  console.log(res);
+};
